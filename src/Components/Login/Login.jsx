e2996c495bea6ffe3a956b23751db232c7cf19e6
RenanCalvis <renancalvis@gmail.com>
2025-02-18 17:03:12 -0400

    Login routes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,24 @@
 // import React from 'react'
 
+import { Route, Routes } from 'react-router-dom';
+
+import LoginForm from './LoginForm';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginCreate from './LoginCreate';
+import LoginPasswordReset from './LoginPasswordReset';
+
 const Login = () => {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/criar" element={<LoginCreate />} />
+        <Route path="/perdeu" element={<LoginPasswordLost />} />
+        <Route path="/resetar" element={<LoginPasswordReset />} />
+      </Routes>
       Logado
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
